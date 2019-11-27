f9ec449c0771f876eeda7eab84a0d5962ea6a534
Daniel Popiniuc <danielpopiniuc@gmail.com>
2019-11-28 00:01:51 +0200

    code optimization

--- project/TypeDetermination.py
+++ project/TypeDetermination.py
@@ -6,34 +6,19 @@ from BasicNeeds import BasicNeeds as _cls_bn
 
 class TypeDetermination:
 
-    importance__low_to_high =     [
-        'empty',
-        'int',
-        'float-USA',
-        'date-iso8601',
-        'date-USA',
-        'time-24',
-        'time-24-us',
-        'time-USA',
-        'time-USA-us',
-        'datetime-iso8601',
-        'datetime-iso8601-us',
-        'str'
-    ]
-
-    @staticmethod
     def fn_analyze_field_content_to_establish_data_type(self,
                                                         field_idx, 
                                                         field_name,
                                                         field_counted_nulls,
                                                         field_unique_values,
                                                         field_panda_type,
+                                                        data_type_and_their_formats_to_evaluate,
                                                         verbose):
         field_structure = []
         # Analyze unique values
         for unique_row_index, current_value in enumerate(field_unique_values):
             # determine the field type by current content
-            crt_field_type = self.fn_type_determination(current_value)
+            crt_field_type = self.fn_type_determination(current_value, data_type_and_their_formats_to_evaluate)
             # write aside the determined value
             if unique_row_index == 0:
                 field_structure = {
@@ -47,9 +32,9 @@ class TypeDetermination:
                                           + f'has the value <{current_value}> '
                                           + f'which mean is of type "{crt_field_type}"')
             else:
-                crt_type_index = self.importance__low_to_high.index(crt_field_type)
+                crt_type_index = list(data_type_and_their_formats_to_evaluate.keys()).index(crt_field_type)
                 prv_type = field_structure['type']
-                prv_type_index = self.importance__low_to_high.index(prv_type)
+                prv_type_index = list(data_type_and_their_formats_to_evaluate.keys()).index(prv_type)
                 # if CSV structure for current field (column) exists, does the current type is more important?
                 if crt_type_index > prv_type_index:
                     _cls_bn.fn_optional_print(_cls_bn, verbose, f' column {field_idx} having the name [{field_name}] '
@@ -63,7 +48,8 @@ class TypeDetermination:
                 return field_structure
         return field_structure
 
-    def fn_detect_csv_structure(self, input_csv_data_frame, verbose):
+    @staticmethod
+    def fn_detect_csv_structure(self, input_csv_data_frame, formats_to_evaluate, verbose):
         col_idx = 0
         csv_structure = []
         # Cycle through all found columns
@@ -82,6 +68,7 @@ class TypeDetermination:
                                                                                               counted_nulls,
                                                                                               list_unique_values[0:200],
                                                                                               panda_determined_type,
+                                                                                              formats_to_evaluate,
                                                                                               verbose)
             elif panda_determined_type == 'int64':
                 csv_structure.append(col_idx)
@@ -101,44 +88,21 @@ class TypeDetermination:
             counted_values_null = field_content.isnull().sum()
             counted_values_not_null = field_content.notnull().sum()
             counted_values_unique = field_content.nunique()
-            _cls_bn.fn_optional_print(_cls_bn, verbose, f'"{field_name}" has following characteristics: ' + \
-                                     f'count of null values: {counted_values_null}, ' + \
-                                     f'count of not-null values: {counted_values_not_null}, ' + \
-                                     f'count of unique values: {counted_values_unique}, ' + \
-                                     f'list of not-null and unique values is: <' + \
+            _cls_bn.fn_optional_print(_cls_bn, verbose, f'"{field_name}" has following characteristics: ' +
+                                     f'count of null values: {counted_values_null}, ' +
+                                     f'count of not-null values: {counted_values_not_null}, ' +
+                                     f'count of unique values: {counted_values_unique}, ' +
+                                     f'list of not-null and unique values is: <' +
                                      '>, <'.join(np.array(field_unique_values, dtype=str)) + '>')
 
     @staticmethod
-    def fn_type_determination(intput_variable_to_assess):
+    def fn_type_determination(intput_variable_to_assess, evaluation_formats):
         # Website https://regex101.com/ was used to validate below code
-        re_int = '^[+-]*[0-9]+(\\.{1}0*)*$' # final "." or ".0" is still considered as integer
-        re_float_usa = '^[+-]*[0-9]*\\.{1}[0-9]*$'
-        re_date_us = '^([1-9]|11|12|0[0-9]|1[0-2])/([1-9]|0[0-9]|1[0-9]|2[0-9]|3[0-1])/(1[0-9]{3}|2[0-9]{3})$'
-        re_date_iso8601 = '^(1[0-9]{3}|2[0-9]{3})-([0-9]|0[0-9]|1[0-2])-([0-9]|0[0-9]|1[0-9]|2[0-9]|3[0-1])$'
-        re_time24 = '^(2[0-3]|[01][0-9]|[0-9]):([0-5][0-9]|[0-9]):([0-5][0-9]|[0-9])$'
-        re_time_us = '^([0-9]|0[0-9]|1[0-2]):{1}([0-5][0-9]|[0-9]):{1}([0-5][0-9]|[0-9])\\s*(AM|am|PM|pm)$'
         variable_to_assess = str(intput_variable_to_assess)
         if variable_to_assess == '':
             return 'empty'
-        elif re.match(re_int, variable_to_assess):
-            return 'int'
-        elif re.match(re_float_usa, variable_to_assess):
-            return 'float-USA'
-        elif re.match(re_date_iso8601, variable_to_assess):
-            return 'date-iso8601'
-        elif re.match(re_date_us, variable_to_assess):
-            return 'date-USA'
-        elif re.match(re_time24, variable_to_assess):
-            return 'time-24'
-        elif re.match(re_time24.replace(')$', ').([0-9]{6})$'), variable_to_assess):
-            return 'time-24-us'
-        elif re.match(re_time_us, variable_to_assess):
-            return 'time-USA'
-        elif re.match(re_time_us.replace(')$', ').([0-9]{6})$'), variable_to_assess):
-            return 'time-USA-us'
-        elif re.match(re_date_iso8601[:-1] + ' ' + re_time24[1:], variable_to_assess):
-            return 'datetime-iso8601'
-        elif re.match(re_date_iso8601[:-1] + ' ' + re_time24[1:].replace(')$', ').([0-9]{6})$'), variable_to_assess):
-            return 'datetime-iso8601-us'
         else:
+            for current_dtype, current_format in evaluation_formats.items():
+                if re.match(current_format, variable_to_assess):
+                    return current_dtype
             return 'str'
