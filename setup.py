022e20493475b51f79bcbe9ea000dc819e7c2a87
Daniel Popiniuc <danielpopiniuc@gmail.com>
2019-12-04 10:12:57 +0200

    updated setup

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
 import platform
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description_readme = fh.read()
 
-tableau_hyper_api__current_known_version = '0.0.8953'
+this_package_website = 'https://github.com/danielgp/tableau-hyper-management'
+tableau_hyper_website_download = 'http://downloads.tableau.com/tssoftware/tableauhyperapi-'
+tableau_hyper_api__current_known_version = '0.0.8953'  # released on 2019-10-30
+url__tableau_hyper_api = tableau_hyper_website_download + tableau_hyper_api__current_known_version
 
 if platform.system() == 'Windows':
-    url__tableau_hyper_api = 'http://downloads.tableau.com/tssoftware/tableauhyperapi-'\
-                             + tableau_hyper_api__current_known_version \
-                             + '-py3-none-win_amd64.whl'
+    url__tableau_hyper_api += '-py3-none-win_amd64.whl'
 elif platform.system() == 'Darwin':
-    url__tableau_hyper_api = 'http://downloads.tableau.com/tssoftware/tableauhyperapi-'\
-                             + tableau_hyper_api__current_known_version \
-                             + '-py3-none-macosx_10_11_x86_64.whl'
+    url__tableau_hyper_api += '-py3-none-macosx_10_11_x86_64.whl'
 elif platform.system() == 'Linux':
-    url__tableau_hyper_api = 'http://downloads.tableau.com/tssoftware/tableauhyperapi-'\
-                             + tableau_hyper_api__current_known_version \
-                             + '-py3-none-linux_x86_64.whl'
+    url__tableau_hyper_api += '-py3-none-linux_x86_64.whl'
 else:
-    url__tableau_hyper_api = 'http://downloads.tableau.com/tssoftware/tableauhyperapi-'\
-                             + tableau_hyper_api__current_known_version \
-                             + '-py3-none-linux_x86_64.whl'
+    url__tableau_hyper_api += '-py3-none-linux_x86_64.whl'
 
 setup(
     author = 'Daniel Popiniuc',
@@ -52,17 +47,21 @@ setup(
     license = 'LGPL3',
     license_file = 'LICENSE',
     long_description = long_description_readme,
+    long_description_content_type = 'text/markdown',
     name = 'tableau-hyper-management',
     package_data = {
-        '': ['*.json', '*.md']
+        '': [
+            '*.json',
+            '*.md'
+        ]
     },
     packages = find_packages(),
     project_urls = {
-        "Documentation": "https://github.com/danielgp/tableau-hyper-management/blob/master/README.md",
-        "Issue Tracker": "https://github.com/danielgp/tableau-hyper-management/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc",
-        "Source Code": "https://github.com/danielgp/tableau-hyper-management"
+        'Documentation': this_package_website + '/blob/master/README.md',
+        'Issue Tracker': this_package_website + '/issues?q=is%3Aissue+is%3Aopen+sort%3Aupdated-desc',
+        'Source Code': this_package_website
     },
     python_requires = '>=3.6',
-    url = 'https://github.com/danielgp/tableau-hyper-management/releases', # project home page, if any
-    version = '1.0.0'
+    url = this_package_website + '/releases',  # project home page, if any
+    version = '1.0.1'
 )
