76e00005c6d3e103b67cf72aadc5395b6b098255
Daniel Popiniuc <danielpopiniuc@gmail.com>
2019-12-05 21:30:52 +0200

    measures taken to shorten a long code line

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import time
 from datetime import timedelta
 
 # Custom classes specific to this package
-from tableau_hyper_management.CommandLineArgumentsHandling import ClassBN
+from tableau_hyper_management.CommandLineArgumentsHandling import ClsBN
 from tableau_hyper_management.CommandLineArgumentsHandling import \
     CommandLineArgumentsHandling as ClassCLAH
 
@@ -17,12 +17,12 @@ from tableau_hyper_management.CommandLineArgumentsHandling import \
 if __name__ == '__main__':
     # marking the start of performance measuring (in nanoseconds)
     performance_start = time.perf_counter_ns()
-    ClassBN.fn_load_configuration(ClassBN)
+    ClsBN.fn_load_configuration(ClsBN)
     ClassCLAH.fn_command_line_argument_interpretation(ClassCLAH)
     # marking the end of performance measuring (in nanoseconds)
     performance_finish = time.perf_counter_ns()
     # calculate time spent on execution
     performance_timed = timedelta(microseconds=((performance_finish - performance_start) / 1000))
     # display time spent on execution
-    ClassBN.fn_timestamped_print(ClassBN, 'This script has been executed in '
-                                 + format(performance_timed) + ' seconds')
+    ClsBN.fn_timestamped_print(ClsBN, 'This script has been executed in '
+                               + format(performance_timed) + ' seconds')
--- tableau_hyper_management/CommandLineArgumentsHandling.py
+++ tableau_hyper_management/CommandLineArgumentsHandling.py
@@ -17,7 +17,7 @@ import sys
 import pandas as pd
 
 # Custom classes specific to this package
-from .TableauHyperApiExtraLogic import ClassBN
+from .TableauHyperApiExtraLogic import ClassBN as ClsBN
 from .TableauHyperApiExtraLogic import TableauHyperApiExtraLogic as ClassTHAEL
 
 
@@ -25,37 +25,37 @@ class CommandLineArgumentsHandling:
 
     @staticmethod
     def fn_assess_option(current_option, str_meaning, given_default_value):
-        if ClassBN.cfg_dtls['options'][current_option]['default_value'] == given_default_value:
+        if ClsBN.cfg_dtls['options'][current_option]['default_value'] == given_default_value:
             str_feedback_parts = {
                 'prefix': 'Fatal Error',
                 'verdict': 'Expected -' + current_option + '|--'
-                           + ClassBN.cfg_dtls['options'][current_option]['option_long']
+                           + ClsBN.cfg_dtls['options'][current_option]['option_long']
                            + ' <'
-                           + ClassBN.cfg_dtls['options'][current_option]['option_sample_value']
+                           + ClsBN.cfg_dtls['options'][current_option]['option_sample_value']
                            + '> but nothing of that sort has been seen...',
                 'suffix': ':-('
             }
             ln = len(str_feedback_parts['verdict']) - len(str_feedback_parts['prefix'])
-            ClassBN.fn_timestamped_print(ClassBN, str_feedback_parts['prefix'] + '_'*ln)
+            ClsBN.fn_timestamped_print(ClsBN, str_feedback_parts['prefix'] + '_'*ln)
             print(str_feedback_parts['verdict'] + ' ' + str_feedback_parts['suffix'])
             sys.exit(2)
-        ClassBN.fn_timestamped_print(ClassBN, str_meaning + ' is "' + given_default_value + '"')
+        ClsBN.fn_timestamped_print(ClsBN, str_meaning + ' is "' + given_default_value + '"')
 
     @staticmethod
     def fn_build_combined_options():
         str_combined_opts = ''
-        for option_index, crt_opt in enumerate(ClassBN.cfg_dtls['options']):
+        for option_index, crt_opt in enumerate(ClsBN.cfg_dtls['options']):
             if crt_opt is not None:
                 str_option_crt = '-' + crt_opt + '|--'\
-                                 + ClassBN.cfg_dtls['options'][crt_opt]['option_long']
-                if 'option_sample_value' in ClassBN.cfg_dtls['options'][crt_opt]:
+                                 + ClsBN.cfg_dtls['options'][crt_opt]['option_long']
+                if 'option_sample_value' in ClsBN.cfg_dtls['options'][crt_opt]:
                     str_option_crt += ' <'\
-                                      + ClassBN.cfg_dtls['options'][crt_opt]['option_sample_value']\
+                                      + ClsBN.cfg_dtls['options'][crt_opt]['option_sample_value']\
                                       + '>'
-                if ClassBN.cfg_dtls['options'][crt_opt]['option_type'] == 'optional':
+                if ClsBN.cfg_dtls['options'][crt_opt]['option_type'] == 'optional':
                     str_combined_opts += ' [' + str_option_crt\
                                          + '|if omitted, default value will be considered: '\
-                                         + str(ClassBN.cfg_dtls['options'][crt_opt]['default_value'])\
+                                         + str(ClsBN.cfg_dtls['options'][crt_opt]['default_value'])\
                                          + ']'
                 else:
                     str_combined_opts += ' ' + str_option_crt
@@ -64,19 +64,18 @@ class CommandLineArgumentsHandling:
     @staticmethod
     def fn_build_long_options():
         str_opts = []
-        for option_index, crt_lng_opt in enumerate(ClassBN.cfg_dtls['options']):
+        for option_index, crt_lng_opt in enumerate(ClsBN.cfg_dtls['options']):
             if crt_lng_opt is not None:
                 str_opts.append(option_index)
-                str_opts[option_index] = ClassBN.cfg_dtls['options'][crt_lng_opt]['option_long']\
-                                         + '='
+                str_opts[option_index] = ClsBN.cfg_dtls['options'][crt_lng_opt]['option_long'] + '='
         return str_opts
 
     @staticmethod
     def fn_build_short_options():
         str_short_options = 'h'
-        for crt_short_opt in ClassBN.cfg_dtls['options']:
+        for crt_short_opt in ClsBN.cfg_dtls['options']:
             if crt_short_opt is not None:
-                if ClassBN.cfg_dtls['options'][crt_short_opt]['option_type'] == 'mandatory':
+                if ClsBN.cfg_dtls['options'][crt_short_opt]['option_type'] == 'mandatory':
                     str_short_options += crt_short_opt + ':'
                 else:
                     str_short_options += crt_short_opt
@@ -115,7 +114,7 @@ class CommandLineArgumentsHandling:
             else:
                 assert False, "Unhandled Option: " + arg
         self.fn_assess_option('i', 'Input file', input_file)
-        ClassBN.fn_timestamped_print(ClassBN, 'CSV field separator is "'
+        ClsBN.fn_timestamped_print(ClsBN, 'CSV field separator is "'
                                      + csv_field_separator + '"')
         self.fn_assess_option('o', 'Output file', output_file)
         print('#' * 120)
@@ -127,5 +126,5 @@ class CommandLineArgumentsHandling:
                                      encoding='utf-8')
         ClassTHAEL.fn_run_hyper_creation(ClassTHAEL,
                                          csv_content_df,
-                                         ClassBN.cfg_dtls['data_types'],
+                                         ClsBN.cfg_dtls['data_types'],
                                          output_file, verbose)
