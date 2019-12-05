fb1b67d5b5f67d6062363a5c812f7a59d28aae3b
Daniel Popiniuc <danielpopiniuc@gmail.com>
2019-12-05 19:56:51 +0200

    new function added to allow printing with timestamp as prefix

--- tableau_hyper_management/BasicNeeds.py
+++ tableau_hyper_management/BasicNeeds.py
@@ -3,9 +3,17 @@ BasicNeeds - useful functions library
 
 This library has functions useful to keep main logic short and simple
 """
+# standard Python packages
+from datetime import datetime, timezone
+
 
 class BasicNeeds:
 
-    def fn_optional_print(boolean_variable, string_to_print):
+    def fn_optional_print(self, boolean_variable, string_to_print):
         if boolean_variable:
-            print(string_to_print)
+            self.fn_timestamped_print(self, string_to_print)
+
+    @staticmethod
+    def fn_timestamped_print(self, string_to_print):
+        print(datetime.now(timezone.utc).strftime("%Y-%b-%d %H:%M:%S.%f %Z")
+              + ' - ' + string_to_print)
