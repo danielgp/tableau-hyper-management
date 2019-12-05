95f631336c0b84431f80b5801f9ae33d309b542a
Daniel Popiniuc <danielpopiniuc@gmail.com>
2019-12-05 19:59:50 +0200

    internal code optimization by replacing quite a few method parameters to a more compact list

--- tableau_hyper_management/TypeDetermination.py
+++ tableau_hyper_management/TypeDetermination.py
@@ -3,41 +3,41 @@ TypeDetermination - a data type determination library
 
 This library allows data type determination based on data frame content
 """
-
-import numpy as np
+# standard Python packages
 import re
-
-from . import BasicNeeds as ClassBN
+# additional Python packages available from PyPi
+import numpy as np
+# Custom class specific to this package
+from BasicNeeds import BasicNeeds as ClassBN
 
 
 class TypeDetermination:
 
     def fn_analyze_field_content_to_establish_data_type(self,
-                                                        field_idx, 
-                                                        field_name,
-                                                        field_counted_nulls,
-                                                        field_unique_values,
-                                                        field_panda_type,
+                                                        field_characteristics,
                                                         data_type_and_their_formats_to_evaluate,
                                                         verbose):
         field_structure = []
+        instance_bn = ClassBN
         # Analyze unique values
-        for unique_row_index, current_value in enumerate(field_unique_values):
+        for unique_row_index, current_value in enumerate(field_characteristics['unique_values']):
             # determine the field type by current content
             crt_field_type = self.fn_type_determination(current_value,
                                                         data_type_and_their_formats_to_evaluate)
             # write aside the determined value
             if unique_row_index == 0:
                 field_structure = {
-                    'order': field_idx,
-                    'name': field_name,
-                    'nulls': field_counted_nulls,
-                    'panda_type': field_panda_type,
+                    'order': field_characteristics['order'],
+                    'name': field_characteristics['name'],
+                    'nulls': field_characteristics['nulls'],
+                    'panda_type': field_characteristics['panda_type'],
                     'type': crt_field_type
                 }
-                ClassBN.fn_optional_print(verbose,
-                                          f'Column {field_idx} having the name [{field_name}] '
-                                          + f'has the value <{current_value}> '
+                ClassBN.fn_optional_print(instance_bn, verbose,
+                                          'Column ' + str(field_characteristics['order'])
+                                          + ' having the name ['
+                                          + field_characteristics['name']
+                                          + f'] has the value <{current_value}>'
                                           + f'which mean is of type "{crt_field_type}"')
             else:
                 crt_type_index = list(data_type_and_their_formats_to_evaluate.keys()).\
@@ -48,9 +48,11 @@ class TypeDetermination:
                 # if CSV structure for current field (column) exists,
                 # does the current type is more important?
                 if crt_type_index > prv_type_index:
-                    ClassBN.fn_optional_print(verbose,
-                                              f' column {field_idx} having the name [{field_name}] '
-                                              + f'has the value <{current_value}> '
+                    ClassBN.fn_optional_print(instance_bn, verbose,
+                                              'Column ' + str(field_characteristics['order'])
+                                              + ' having the name ['
+                                              + field_characteristics['name']
+                                              + f'] has the value <{current_value}> '
                                               + f'which means is of type "{crt_field_type}" '
                                               + 'and this is stronger than previously thought '
                                               + f'to be as "{prv_type}"')
@@ -60,27 +62,30 @@ class TypeDetermination:
                 return field_structure
         return field_structure
 
-    @staticmethod
     def fn_detect_csv_structure(self, input_csv_data_frame, formats_to_evaluate, verbose):
         col_idx = 0
         csv_structure = []
         # Cycle through all found columns
         for label, content in input_csv_data_frame.items():
             panda_determined_type = content.infer_objects().dtypes
-            ClassBN.fn_optional_print(verbose, f'Field "{label}" according to Pandas package '
+            ClassBN.fn_optional_print(self, verbose,
+                                      f'Field "{label}" according to Pandas package '
                                       + f'is of type "{panda_determined_type}"')
             counted_nulls = content.isnull().sum()
             if panda_determined_type in ('float64', 'object'):
-                list_unique_vals = content.dropna().unique()
-                self.fn_optional_column_statistics(self, verbose, label, content, list_unique_vals)
+                list_unique_values = content.dropna().unique()
+                self.fn_optional_column_statistics(verbose, label, content,
+                                                   list_unique_values)
+                preliminary_list = {
+                    'order': col_idx,
+                    'name': label,
+                    'nulls': counted_nulls,
+                    'panda_type': panda_determined_type,
+                    'unique_values': list_unique_values[0:200]
+                }
                 csv_structure.append(col_idx)
                 csv_structure[col_idx] = self.\
-                    fn_analyze_field_content_to_establish_data_type(self,
-                                                                    col_idx,
-                                                                    label,
-                                                                    counted_nulls,
-                                                                    list_unique_vals[0:200],
-                                                                    panda_determined_type,
+                    fn_analyze_field_content_to_establish_data_type(self, preliminary_list,
                                                                     formats_to_evaluate,
                                                                     verbose)
             elif panda_determined_type == 'int64':
@@ -96,26 +101,26 @@ class TypeDetermination:
         return csv_structure
 
     @staticmethod
-    def fn_optional_column_statistics(self, verbose, field_name, field_content, field_unique_vals):
+    def fn_optional_column_statistics(verbose, field_name, field_content, field_unique_values):
         if verbose:
             counted_values_null = field_content.isnull().sum()
             counted_values_not_null = field_content.notnull().sum()
             counted_values_unique = field_content.nunique()
             ClassBN.fn_optional_print(verbose, f'"{field_name}" has following characteristics: ' +
-                                     f'count of null values: {counted_values_null}, ' +
-                                     f'count of not-null values: {counted_values_not_null}, ' +
-                                     f'count of unique values: {counted_values_unique}, ' +
-                                     f'list of not-null and unique values is: <' +
-                                     '>, <'.join(np.array(field_unique_vals, dtype=str)) + '>')
+                                      f'count of null values: {counted_values_null}, ' +
+                                      f'count of not-null values: {counted_values_not_null}, ' +
+                                      f'count of unique values: {counted_values_unique}, ' +
+                                      f'list of not-null and unique values is: <' +
+                                      '>, <'.join(np.array(field_unique_values, dtype=str)) + '>')
 
     @staticmethod
-    def fn_type_determination(intput_variable_to_assess, evaluation_formats):
+    def fn_type_determination(input_variable_to_assess, evaluation_formats):
         # Website https://regex101.com/ was used to validate below code
-        variable_to_assess = str(intput_variable_to_assess)
+        variable_to_assess = str(input_variable_to_assess)
         if variable_to_assess == '':
             return 'empty'
         else:
-            for current_dtype, current_format in evaluation_formats.items():
+            for current_data_type, current_format in evaluation_formats.items():
                 if re.match(current_format, variable_to_assess):
-                    return current_dtype
+                    return current_data_type
             return 'str'
