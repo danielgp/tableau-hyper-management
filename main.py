3c429c49d5e1fe4b4ecfb5e3926cf0b54f85e6e6
Daniel Popiniuc <danielpopiniuc@gmail.com>
2019-12-15 00:37:20 +0200

    switch to a more standardized class name in the main logic

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from datetime import timedelta
 import pandas as pd
 
 # Custom classes specific to this package
-from tableau_hyper_management.TypeDetermination import ClassBN as ClsBN
+from tableau_hyper_management.TypeDetermination import ClassBN
 from tableau_hyper_management.TypeDetermination import TypeDetermination as ClassTD
 from tableau_hyper_management.CommandLineArgumentsManagement import \
     CommandLineArgumentsManagement as ClassCLAM
@@ -23,15 +23,15 @@ from tableau_hyper_management.TableauHyperApiExtraLogic import \
 if __name__ == '__main__':
     # marking the start of performance measuring (in nanoseconds)
     performance_start = time.perf_counter_ns()
-    ClsBN.fn_load_configuration(ClsBN)
-    parameters_interpreted = ClassCLAM.parse_arguments(ClassCLAM, ClsBN.cfg_dtls['options'])
+    ClassBN.fn_load_configuration(ClassBN)
+    parameters_interpreted = ClassCLAM.parse_arguments(ClassCLAM, ClassBN.cfg_dtls['options'])
     print('~'*100)
-    ClsBN.fn_timestamped_print(ClsBN, 'Input file is ' + parameters_interpreted.input_file)
-    ClsBN.fn_timestamped_print(ClsBN, 'CSV field separator is '
+    ClassBN.fn_timestamped_print(ClassBN, 'Input file is ' + parameters_interpreted.input_file)
+    ClassBN.fn_timestamped_print(ClassBN, 'CSV field separator is '
                                + parameters_interpreted.csv_field_separator)
-    ClsBN.fn_timestamped_print(ClsBN, 'Unique values to analyze is limited to '
+    ClassBN.fn_timestamped_print(ClassBN, 'Unique values to analyze is limited to '
                                + str(parameters_interpreted.unique_values_to_analyze_limit))
-    ClsBN.fn_timestamped_print(ClsBN, 'Output file is ' + parameters_interpreted.output_file)
+    ClassBN.fn_timestamped_print(ClassBN, 'Output file is ' + parameters_interpreted.output_file)
     print('~' * 100)
     # initiate Data Frame from specified CSV file
     csv_content_df = pd.read_csv(filepath_or_buffer=parameters_interpreted.input_file,
@@ -51,5 +51,5 @@ if __name__ == '__main__':
     # calculate time spent on execution
     performance_timed = timedelta(microseconds=((performance_finish - performance_start) / 1000))
     # display time spent on execution
-    ClsBN.fn_timestamped_print(ClsBN, 'This script has been executed in '
-                               + format(performance_timed) + ' seconds')
+    ClassBN.fn_timestamped_print(ClassBN, 'This script has been executed in '
+                                 + format(performance_timed) + ' seconds')
