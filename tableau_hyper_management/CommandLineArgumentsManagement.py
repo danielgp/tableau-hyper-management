79b53366ff9f4352a57ec810b0aecb3312671bea
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-01-20 22:11:39 +0100

    revised implementing verbose application parameter

--- tableau_hyper_management/CommandLineArgumentsManagement.py
+++ tableau_hyper_management/CommandLineArgumentsManagement.py
@@ -13,7 +13,7 @@ class CommandLineArgumentsManagement:
     def parse_arguments(self, configuration_details):
         parser = argparse.ArgumentParser()
         for input_key, attributes in configuration_details.items():
-            action_value = self.translate_default(attributes['default_value'])
+            action_value = self.translate_default_to_action(attributes['default_value'])
             if action_value is None:
                 parser.add_argument('-' + input_key, '--' + attributes['option_long'],
                                     required=attributes['option_required'],
@@ -24,10 +24,11 @@ class CommandLineArgumentsManagement:
                                     required=attributes['option_required'],
                                     default=attributes['default_value'],
                                     action=action_value)
+        parser.add_argument('-v', '--verbose', required=False, default=False, action='store_true')
         return parser.parse_args()
 
     @staticmethod
-    def translate_default(given_default_value):
+    def translate_default_to_action(given_default_value):
         if given_default_value == True:
             return 'store_true'
         elif given_default_value == False:
