4fcd559df22196d097862a0737edcdcc6a7e1e07
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-04-05 03:57:07 +0300

    moved couple of time performance calls to a a deeper level

--- tableau_hyper_management/TableauHyperApiExtraLogic.py
+++ tableau_hyper_management/TableauHyperApiExtraLogic.py
@@ -20,7 +20,8 @@ from tableauhyperapi import HyperProcess, Telemetry, \
 
 class TableauHyperApiExtraLogic:
 
-    def fn_build_hyper_columns_for_csv(self, logger, detected_csv_structure):
+    def fn_build_hyper_columns_for_csv(self, logger, timmer, detected_csv_structure):
+        timmer.start()
         list_to_return = []
         for current_field_structure in detected_csv_structure:
             list_to_return.append(current_field_structure['order'])
@@ -37,6 +38,8 @@ class TableauHyperApiExtraLogic:
                 type=current_column_type,
                 nullability=nullability_value
             )
+        logger.info('Building Hyper columns completed')
+        timmer.stop()
         return list_to_return
 
     @staticmethod
@@ -63,18 +66,11 @@ class TableauHyperApiExtraLogic:
 
     def fn_create_hyper_file_from_csv(self, local_logger, timmer, input_csv_data_frame,
                                       in_data_type, given_parameters):
-        timmer.start()
-        hyper_cols = self.fn_build_hyper_columns_for_csv(local_logger, in_data_type)
-        local_logger.info('Building Hyper columns completed')
-        timmer.stop()
+        hyper_cols = self.fn_build_hyper_columns_for_csv(local_logger, timmer, in_data_type)
         # The rows to insert into the <hyper_table> table.
-        timmer.start()
-        data_to_insert = self.fn_rebuild_csv_content_for_hyper(local_logger,
+        data_to_insert = self.fn_rebuild_csv_content_for_hyper(local_logger, timmer,
                                                                input_csv_data_frame,
                                                                in_data_type)
-        local_logger.info('Re-building CSV content for maximum Hyper compatibility '
-                          + 'has been completed')
-        timmer.stop()
         # Starts the Hyper Process with telemetry enabled/disabled to send data to Tableau or not
         # To opt in, simply set telemetry=Telemetry.SEND_USAGE_DATA_TO_TABLEAU.
         # To opt out, simply set telemetry=Telemetry.DO_NOT_SEND_USAGE_DATA_TO_TABLEAU.
@@ -116,7 +112,8 @@ class TableauHyperApiExtraLogic:
             local_logger.info('Connection to the Hyper engine file has been closed')
         local_logger.info('Hyper engine process has been shut down')
 
-    def fn_rebuild_csv_content_for_hyper(self, logger, input_df, detected_fields_type):
+    def fn_rebuild_csv_content_for_hyper(self, logger, timmer, input_df, detected_fields_type):
+        timmer.start()
         input_df.replace(to_replace=[numpy.nan], value=[None], inplace=True)
         # Cycle through all found columns
         for current_field in detected_fields_type:
@@ -128,6 +125,9 @@ class TableauHyperApiExtraLogic:
                 input_df[fld_nm] = input_df[fld_nm].fillna(0).astype('int64')
             elif current_field['type'][0:5] in ('date-', 'datet', 'time-'):
                 input_df[fld_nm] = self.fn_string_to_date(fld_nm, input_df)
+        logger.info('Re-building CSV content for maximum Hyper compatibility '
+                          + 'has been completed')
+        timmer.stop()
         return input_df.values
 
     def fn_run_hyper_creation(self, local_logger, timmer, input_data_frame, input_data_type,
