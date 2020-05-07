e86c2e2fe89058fd0dd36040c580adfd2b6bbf9d
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-05-07 16:59:15 +0300

    implementing tox testing

--- sources/localizations_setup.py
+++ sources/localizations_setup.py
@@ -2,7 +2,7 @@
 localization_setup - facilitates localization activities
 """
 # facilitate dependencies management
-from setuptools import setup, find_packages
+from setuptools import setup
 # facilitate internationalization
 from babel.messages import frontend
 
