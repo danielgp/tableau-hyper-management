4bbbf059ad1d10cc96e1c44d2715f56a23137297
Daniel Popiniuc <danielpopiniuc@gmail.com>
2020-05-07 15:07:20 +0300

    internal code optimization for localization compile script

--- sources/localizations_compile.py
+++ sources/localizations_compile.py
@@ -9,9 +9,36 @@ import platform
 
 
 class CustomizedLocalizationCompiling:
+    localisation_compilation_is_required = False
 
-    def compile_localisation_files(self, compilation_needed, domains_to_compile):
-        if compilation_needed:
+    def check_file_pairs(self, in_dict):
+        get_domain_details_for_compilation = False
+        if os.path.lexists(pathlib.Path(in_dict['compiled'])):
+            source_last_modified = os.path.getmtime(in_dict['source'])
+            compiled_last_modified = os.path.getmtime(in_dict['compiled'])
+            if source_last_modified > compiled_last_modified:
+                self.localisation_compilation_is_required = True
+                get_domain_details_for_compilation = True
+                print('#' + str(in_dict['counter']) + ', '
+                      + 'For locale ' + in_dict['locale'] + ' source file '
+                      + os.path.basename(in_dict['source'])
+                      + ' is newer than compiled one '
+                      + os.path.basename(in_dict['compiled'])
+                      + ' so compilation needs to take place to remedy this')
+                print('===>' + in_dict['source'] + ' has ' + str(source_last_modified)
+                      + ' vs. ' + str(compiled_last_modified))
+        else:
+            self.localisation_compilation_is_required = True
+            get_domain_details_for_compilation = True
+            print('#' + str(in_dict['counter']) + ', '
+                  + 'The file ' + os.path.basename(in_dict['source'])
+                  + ' does not have compiled file for ' + in_dict['locale']
+                  + ' therefore will require compiling into the same folder: '
+                  + os.path.dirname(in_dict['source']))
+        return get_domain_details_for_compilation
+
+    def compile_localisation_files(self, domains_to_compile):
+        if self.localisation_compilation_is_required:
             for current_domain_to_compile in domains_to_compile:
                 os.system(self.get_virtual_environment_python_binary() + ' '
                           + os.path.join(os.path.dirname(__file__), 'localizations_setup.py')
@@ -26,50 +53,33 @@ class CustomizedLocalizationCompiling:
 
     def evaluate_compilation_necessity(self, in_list_localisation_source_files):
         list_size = len(in_list_localisation_source_files)
-        localisation_compilation_is_required = False
+        self.localisation_compilation_is_required = False
         file_list_paring_complete = False
         file_counter = 0
         domains_to_compile = []
         compiling_files_counter = 0
         while not file_list_paring_complete:
-            get_domain_details_for_compilation = False
             source_localisation_file = in_list_localisation_source_files[file_counter]
-            compiled_localisation_file = source_localisation_file.replace('.po', '.mo')
             folder_parts = pathlib.PurePath(source_localisation_file).parts
             current_locale = folder_parts[(len(folder_parts) - 3)]
-            if os.path.lexists(pathlib.Path(compiled_localisation_file)):
-                source_last_modified = os.path.getmtime(source_localisation_file)
-                compiled_last_modified = os.path.getmtime(compiled_localisation_file)
-                if source_last_modified > compiled_last_modified:
-                    localisation_compilation_is_required = True
-                    get_domain_details_for_compilation = True
-                    print('#' + str(file_counter) + ', '
-                          + 'For locale ' + current_locale + ' source file '
-                          + os.path.basename(source_localisation_file)
-                          + ' is newer than compiled one '
-                          + os.path.basename(compiled_localisation_file)
-                          + ' so compilation needs to take place to remedy this')
-                    print('===>' + source_localisation_file + ' has ' + str(source_last_modified)
-                          + ' vs. ' + str(compiled_last_modified))
-            else:
-                get_domain_details_for_compilation = True
-                print('#' + str(file_counter) + ', '
-                      + 'The file ' + os.path.basename(source_localisation_file)
-                      + ' does not have compiled file for ' + current_locale
-                      + ' therefore will require compiling into the same folder: '
-                      + os.path.dirname(source_localisation_file))
-                localisation_compilation_is_required = True
+            fn_dict = {
+                'compiled': source_localisation_file.replace('.po', '.mo'),
+                'counter': file_counter,
+                'locale': current_locale,
+                'source': source_localisation_file,
+            }
+            get_domain_details_for_compilation = self.check_file_pairs(fn_dict)
             if get_domain_details_for_compilation:
                 domains_to_compile.append(compiling_files_counter)
                 domains_to_compile[compiling_files_counter] = {
-                    'input-file': source_localisation_file,
-                    'output-file': compiled_localisation_file,
-                    'locale': current_locale,
+                    'input-file': fn_dict['source'],
+                    'output-file': fn_dict['compiled'],
+                    'locale': fn_dict['locale'],
                 }
                 compiling_files_counter += 1
             file_counter += 1
             file_list_paring_complete = self.file_counter_limit(file_counter, list_size)
-        return localisation_compilation_is_required, domains_to_compile
+        return domains_to_compile
 
     @staticmethod
     def file_counter_limit(in_file_counter, in_file_list_size):
@@ -112,6 +122,5 @@ class CustomizedLocalizationCompiling:
 my_class = CustomizedLocalizationCompiling()
 
 locale_source_files = my_class.get_project_localisation_source_files()
-compilation_is_required, compiling_domains = \
-    my_class.evaluate_compilation_necessity(locale_source_files)
-my_class.compile_localisation_files(compilation_is_required, compiling_domains)
+compiling_domains = my_class.evaluate_compilation_necessity(locale_source_files)
+my_class.compile_localisation_files(compiling_domains)
